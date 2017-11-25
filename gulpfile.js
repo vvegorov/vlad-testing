3dbeb5c47fe7e701710e5b1bf8634297f3c7467b
vvegorov <egorovvvsls@gmail.com>
2017-11-25 15:35:59 +0300

    4 step

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
+
+
+
+
+/* -------- Server  -------- */
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      port: 9000,
+      baseDir: "build"
+    }
+  });
+
+  gulp.watch('build/**/*').on('change', browserSync.reload);
+});
